46c9fb241baea8ba45e2b237a36d8bd5d64279b0
nexovir <nexoovir@gmail.com>
2025-11-20 19:15:48 +0330

    some improvements

--- url_monitor/tasks.py
+++ url_monitor/tasks.py
@@ -675,7 +675,7 @@ def url_monitor(self):
         # discover_urls_task.s('new'),
         # discover_parameter_task.si('new'),
         # fuzz_parameters_on_urls_task.si('new'), # Recommand to do not use it !
-        # vulnerability_monitor_task.si('new'),
+        vulnerability_monitor_task.si('new'),
 
         discover_urls_task.si('available'),
         discover_parameter_task.si('available'),
